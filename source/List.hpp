80833ea1906d1fcc89b3c2d2ac1235d079b8bb8e
herrschaften <zsagar@hotmail.de>
2016-06-06 14:08:52 +0200

    îch habe fertig

--- source/List.hpp
+++ source/List.hpp
@@ -1,51 +1,379 @@
 # ifndef BUW_LIST_HPP
 # define BUW_LIST_HPP
-# include < cstddef >
+# include <cstddef>
+
 // List . hpp
-template < typename T >
-struct List ;
-template < typename T >
-struct ListNode
-{
-ListNode () : m_value () , m_prev ( nullptr ) , m_next ( nullptr ) {}ListNode ( T const & v , ListNode * prev , ListNode * next )
-: m_value ( v ) , m_prev ( prev ) , m_next ( next )
-{}
-T m_value ;
-ListNode * m_prev ;
-ListNode * m_next ;
+template <typename T>
+struct List;
+
+//  prev<-NODE->next mit value 
+template <typename T >
+struct ListNode{
+	ListNode(): 
+	m_value() , 
+	m_prev(nullptr) , 
+	m_next (nullptr) {}
+
+	ListNode (T const& v , ListNode* prev , ListNode* next ): 
+	m_value(v) , 
+	m_prev (prev) , 
+	m_next (next){}
+
+	T m_value ;
+	ListNode* m_prev ;
+	ListNode* m_next ;
 };
-template < typename T >
-struct ListIterator
-{
-friend class List <T >;
+
+//----------------------------------------------------------------------
+template <typename T>
+struct ListIterator{
+
+	typedef ListIterator<T> Self;
+
+	typedef T value_type;
+	typedef T* pointer;
+	typedef T& reference;
+	typedef ptrdiff_t difference_type;
+	typedef std::forward_iterator_tag iterator_category;
+
+	friend class List <T>;
+
+	//default iterator (leere liste)
+	ListIterator() : m_node(nullptr) {}  
+
+	//iterator läuft mit node bis eingegebenes n 
+	ListIterator(ListNode <T>* n) : m_node(n) {}  
+
+	//
+	reference operator*() const{return(m_node->m_value);} 
+
+	//
+	pointer operator->() const{return &(m_node->m_value);} 
+
+	//++a
+	Self& operator++() {
+
+		*this= next(); 
+		return *this;
+	} 
+
+	//a++
+	Self operator++(int) {
+
+		Self tmp = *this;
+		++(*this);
+		return tmp;
+	} 
+
+	//gleich
+	bool operator ==(const Self & x) const {return m_node == x.m_node;} 
+ 
+	//nicht gleich
+	bool operator != (const Self & x ) const {return m_node != x.m_node;}
+
+	//
+	Self next() const{
+
+		if (m_node)
+			return ListIterator(m_node-> m_next);
+		else
+			return ListIterator(nullptr);
+	}
+
+	Self prev() const{
+
+		if (m_node)
+			return ListIterator(m_node-> m_prev);
+		else
+			return ListIterator(nullptr);
+	}
 // not implemented yet
-private :
-ListNode <T >* m_node = nullptr ;
+private:
+	ListNode <T>* m_node = nullptr;
 };
+
+//---------------------------------------------------------------
 template < typename T >
-struct List Const Iterat or
-{
-friend class List <T >;
-public :
+struct ListConstIterator{
+
+	friend class List <T>;
+
+public:
 // not implemented yet
-private :
-ListNode <T >* m_node = nullptr ;
+
+
+private:
+	ListNode <T>* m_node = nullptr;
 };
+
+//----------------------------------------------------------------
 template < typename T >
-class List
-{
-public :
-typedef T value_type ;
-typedef T * pointer ;
-typedef const T * const_pointer ;
-typedef T & reference ;
-typedef const T & const_reference ;
-typedef ListIterator <T > iterator ;
-typedef ListConstIterator <T > const_iterator ;
-friend class ListIterator <T >;
-friend class ListConstIterator <T >;// not implemented yet
-private :
-std :: size_t m_size = 0;
-ListNode <T >* m_first = nullptr ;
-ListNode <T >* m_last = nullptr ;
-};
+class List{
+
+public:
+	typedef T value_type;
+	typedef T* pointer;
+	typedef const T* const_pointer;
+	typedef T& reference;
+	typedef const T& const_reference;
+	typedef ListIterator <T> iterator;
+	typedef ListConstIterator <T> const_iterator;
+	friend class ListIterator <T>;
+	friend class ListConstIterator <T>;// not implemented yet
+
+	List(): 
+		m_size(0), 
+		m_first(nullptr), 
+		m_last(nullptr) 
+		{}
+
+	// copy konstruktor: List<int> l{l1} kopiert l1 in l
+	List<T>(List<T> const& l): 
+		m_size(0), 
+		m_first(nullptr), 
+		m_last(nullptr) 
+		{
+			for (auto it=l.begin(); it != l.end(); it++)
+			{
+				push_back(*it);
+			}
+		}
+
+	//Move Construktor (&& move operator ?)
+	List<T>(List<T> && l): 
+		//werte aus list 
+		m_size(l.m_size),
+		m_first(l.m_first),
+	  	m_last(l.m_last){
+
+		l.m_size=0;
+		l.m_first=nullptr;
+		l.m_last=nullptr;
+		
+		}	
+	
+	
+	~List(){clear();} 
+
+	bool empty () const{
+
+		return size() == 0;
+	}
+
+	std::size_t size () const{
+
+		return m_size;
+	}
+
+	void push_front(T const&  v) {
+
+		if(empty()){
+			ListNode <T>* newleft= new ListNode<T> {v,  nullptr, nullptr};
+			m_first=newleft;
+			m_last=newleft;
+		
+		}
+		else{
+
+			ListNode <T>* newleft= new ListNode<T> {v,  nullptr, m_first};
+			//prev von erstem wird zu newleft
+			m_first->m_prev=newleft;
+			//new left wird zu erstem 
+			m_first=newleft;
+			
+
+		}
+		m_size++;
+	}
+
+	void push_back(T const& v){
+
+		if(empty()){
+			//syntax neuer knoten: Listnode <T>* name= new Listnode <T> {wert, prev knoten, next knoten}
+			ListNode <T>* newlast= new ListNode<T> {v,  nullptr, nullptr};
+			m_first=newlast;
+			m_last=newlast;
+
+		}
+	
+		else{
+
+			ListNode <T>* newlast= new ListNode<T> {v,  m_last, nullptr};
+			//next von last wird neues last
+			m_last->m_next=newlast;
+			m_last=newlast;
+		}
+		m_size++;
+	}
+
+	void pop_front(){
+
+		if(m_size>1){	
+			auto x= m_first->m_next;
+			delete m_first;
+			x->m_prev=nullptr;
+			m_first=x;
+			m_size--;
+		}
+		else if(m_size==1){
+			delete m_first;
+			m_first=nullptr;
+			m_last=nullptr;
+			m_size--;
+		}
+	}	
+
+	void pop_back(){
+
+		if(m_size>1){
+
+			auto x = m_last->m_prev;
+			delete m_last;
+			x->m_next=nullptr;
+			m_last=x;
+			m_size--;
+		}
+		else if(m_size==1){
+			delete m_first;
+			m_first=nullptr;
+			m_last=nullptr;
+			m_size--;
+
+
+		}
+	}
+
+	T front(){
+
+		return m_first->m_value;
+	}
+
+	T last(){
+
+		return m_last->m_value;
+	}
+
+	void clear(){
+		while(!(empty())){
+			pop_back();
+		}
+	}
+
+	iterator begin() const{
+
+		return m_first;
+	}
+
+	iterator end() const{
+
+		//letztes element zeigt auf nullptr like defaultconstruktor
+		return ListIterator<T>();
+	}
+
+	//insert(zeiger, value)
+	iterator insert(iterator it, T const & v ){
+
+		//zeiger am anfang wird einfach davor geschoben mit push front
+		if(it==begin()){
+			push_front(v);
+			return begin();
+		}
+		//zeiger am ende mit push back | begin=1 end=x 1|2|3|x  m_first=1 m_last=3
+		else if(it==end()){
+			push_back(v);
+			return m_last;
+		}
+		else{
+
+			ListNode <T>* newnode= new ListNode <T> {v, it.prev().m_node, it.m_node};
+			
+			//zeiger von el. vor node auf new node (next)
+			it.prev().m_node->m_next=newnode;
+			//zeiger von node auf new node (prev)
+			it.m_node->m_prev=newnode;
+			return newnode;
+		}
+	}
+	
+	void reverse(){
+
+		//startet hinten geht nach vorne(next) bis nullptr erreicht
+		for(auto x=m_last; x!=nullptr; x=x->m_next){
+
+			//zeiger werden vertauscht
+			std::swap(x->m_prev, x->m_next);
+
+		}
+		//start, endpunkt wird vertauscht
+		std::swap(m_first, m_last);
+	}
+
+
+	
+
+	
+
+private:
+	std::size_t m_size = 0;
+	ListNode <T >* m_first = nullptr ;
+	ListNode <T >* m_last = nullptr ;
+
+};	
+
+template <typename T>
+bool operator==(List<T> const& xs, List<T> const& ys){
+
+	//iterator auf beginn der Listen zählen mit ++ durch (später)
+	ListIterator<T> itx = xs.begin(); 
+	ListIterator<T> ity = ys.begin(); 
+
+	//wird nur geändert zwei werte ungleich sonst bleibt es true
+	bool op = true;
+	
+
+	//länge vergleichen
+	if(xs.size() != ys.size()){ 
+
+		op=false;
+	}
+
+
+	//jedes element vergleichen
+	else{
+		unsigned int grenze=0;
+		//so lange elemente gleich und grenze kleiner liste weiter 
+		while ((op==true)&(grenze<(xs.size()))){
+			//abbruch wenn aktuellen elemente !=
+			if(*itx != *ity){
+				op = false;
+			}
+			else {
+				//iterator weiter
+				ity++;
+				itx++; 
+			}
+				//grenze weiter
+				grenze++;
+		}
+	}
+	return op;
+}
+
+template <typename T>
+bool operator!=(List<T> const& xs, List<T> const& ys){
+
+	return !(xs==ys);
+}
+
+//dreht Liste um mit reverse
+template <typename T> List <T> reverse(List <T> rev){
+
+	rev.reverse();
+	return rev;
+}
+
+
+//-------------------------------------------------------------------
+
+
+#endif
--- source/tests.cpp
+++ source/tests.cpp
@@ -1,6 +1,315 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 #include <cmath>
+#include "List.hpp"
+
+
+TEST_CASE ("diesdas","[1]"){
+
+	List<int> l;
+
+	REQUIRE(l.empty());
+
+	REQUIRE(l.size()==0);
+
+	
+
+
+}
+
+TEST_CASE ("push it","[2]"){
+
+	List < int > list ;
+
+	list.push_front (42);
+
+	REQUIRE(42 == list.front());
+
+	list.push_front (4);
+
+	REQUIRE(4 == list.front());	
+	REQUIRE(list.size()==2);
+
+}
+
+
+TEST_CASE ("push back","[2]"){
+
+	List<int> list;
+
+	list.push_back(3);
+
+	REQUIRE(3 == list.last());
+
+	list.push_back(2);
+
+	REQUIRE(2 == list.last());
+}
+
+
+TEST_CASE ("pop it","[2]"){
+
+	List<int> list;
+
+	list.push_front(4);
+	
+	
+	list.pop_front();
+	REQUIRE(list.empty());
+
+	list.push_front(3);
+	REQUIRE(list.size()==1);
+
+	list.push_front(3);
+	REQUIRE(list.size()==2);
+
+	REQUIRE(list.front()==3);
+
+	list.push_back(5);
+	REQUIRE(list.last()==5);
+
+	list.pop_front();
+	REQUIRE(list.size()==2);
+
+	list.pop_back();
+	REQUIRE(list.size()==1);
+
+}
+
+TEST_CASE ("pop back","[2]"){
+
+	List<int> list;
+
+	list.push_front(3);
+
+	list.pop_back();
+	REQUIRE(list.empty());
+
+	list.push_back(4);
+	list.push_back(2);
+
+	list.pop_back();
+
+	REQUIRE(list.last()==4);
+}
+
+TEST_CASE("cleardiss" "[3]"){
+	List<int> l;
+
+	l.push_back(2);
+	l.push_back(3);
+	l.push_back(4);
+	l.clear();
+
+	REQUIRE(l.empty());
+}
+
+TEST_CASE("iteration" "[4]"){
+
+	List<int> l;
+	l.push_back(1);
+	auto i=l.begin();
+	REQUIRE(*i==1);
+	//REQUIRE(ListIterator()==nullptr);
+
+
+//TESTS ÜBERARBEITEN
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!	
+
+	int a = 3;
+	int b = a++;
+	REQUIRE(b==3);
+	REQUIRE(a==4);
+
+	int c = ++a;
+	REQUIRE(c==5);
+	REQUIRE(a==5);
+
+	l.push_back(2);
+	l.push_back(3);
+	l.push_back(4);
+
+
+
+	int Summe = 0;
+
+	for(int i : l){
+
+		Summe+= i;
+
+	}
+
+	REQUIRE(Summe==10);
+
+
+}
+
+
+
+TEST_CASE ( " should be an empty range after default construction " ,"[5]" ){
+
+	List <int> list ;
+	auto b =list.begin();
+	auto e =list.end();
+	REQUIRE (b == e);
+
+}
+
+TEST_CASE ( " provide acces to the first element with begin " , "[5]" ){
+
+	List<int> list ;
+	list.push_front(42);
+	REQUIRE(42 == *list.begin());
+
+}
+
+TEST_CASE("gleichungleich" "[6]"){
+
+
+		List<int>listx ;
+		List<int>listy ;
+		List<int>listz ;
+		for (int i=0; i<10; i++)
+		{ 
+			listz.push_back(i);
+				
+		}
+
+		//gleich leere liste
+		REQUIRE(listx==listy);
+		
+
+		//x und y mit 1 bis 10 füllen
+		for (int i=0; i<10; i++)
+		{ 
+			listx.push_back(i);
+			listy.push_back(i);
+			REQUIRE(listx==listy);		
+		}
+
+		REQUIRE(listx == listy);
+		REQUIRE(listz == listy);
+		//UNgleichheit:
+		listy.push_back(1);
+		REQUIRE((listx!=listy)); 
+		listx.push_back(2);
+		REQUIRE((listx!=listy)); 
+
+		listx.pop_back();
+		listy.pop_back();
+		listy.pop_back();
+		listy.push_back(1);
+		REQUIRE((listx!=listy)); 
+		listy.clear();
+		listx.clear();
+
+		for (int i=1; i<10; i++)
+		{ 
+			listx.push_back(i);
+			listy.push_back(2*i);
+			REQUIRE(listx!=listy);		
+		}			
+}
+
+
+
+TEST_CASE ( " copy constructor" , " [7] " )
+{
+List < int > list ;
+list . push_front (1);
+list . push_front (2);
+list . push_front (3);
+list . push_front (4);
+List < int > list2 { list };
+REQUIRE ( list == list2 );
+
+//test ob wirklich verschiedene listen
+list.pop_front();
+REQUIRE(list2.front()==4);
+}
+
+
+TEST_CASE("insert", "[8]"){
+
+
+	List<int> l;
+	List<int> l1;
+
+	auto it=l.begin();
+	
+	l.insert(it, 2);
+
+	REQUIRE(l.front()==2);
+
+	for(int i=0; i<6; i++){
+		l1.push_back(i);
+
+	}
+
+	auto dis=l1.begin();
+
+
+	l1.insert(dis, 9);
+
+
+	for(auto i: l1){
+		//l1.push_back(i);
+		std::cout<< i <<std::endl;
+	}
+
+	REQUIRE(l1.front()==9);
+	REQUIRE(l1.last()==5);
+}
+
+
+TEST_CASE ("reverse", "[9]"){
+
+	List<int> l;
+
+	for(int i=1; i<6; i++){
+
+		l.push_back(i);
+	}
+
+	l.reverse();
+	
+	REQUIRE(l.front()==5);
+	REQUIRE(l.last()==1);
+
+
+}	
+
+TEST_CASE ("move constructor", "[12]"){
+
+	List < int > list ;
+	list.push_front (1);
+	list.push_front (2);
+	list.push_front (3);
+	list.push_front (4);
+	List<int>list2(std::move(list));
+	REQUIRE (0==list.size());
+	REQUIRE (list.empty());
+	REQUIRE (4==list2.size());
+
+}
+
+
+TEST_CASE("liste in vek", "[10]") {
+  	List<int> l;
+	for(int i=1; i<6; i++){l.push_back(i);}
+
+
+  std::vector<unsigned int> vek(l.size());
+  std::copy(l.begin(), l.end(), std::begin(vek));
+
+  REQUIRE(1 == vek[0]);
+  REQUIRE(2 == vek[1]);
+  REQUIRE(3 == vek[2]);
+  REQUIRE(4 == vek[3]);
+  REQUIRE(5 == vek[4]);
+}
+
 
 
 
@@ -8,6 +317,8 @@
 int main(int argc, char * argv[]) {
 
   return Catch::Session().run(argc, argv);
+
+
 
 }
 
